5ae411bf2d48de65968ec4574d1462ff4b52ec16
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-10-07 20:03:22 +0530

    added the code for reading and writing to redis cache using websocket

---
+++ src/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const {createServer} = require("http");
+const {Server} = require("socket.io");
+const Redis = require('ioredis');
+const bodyParser = require('body-parser');
+
+
+const app=express();
+app.use(bodyParser.json());
+const httpServer = createServer(app);
+
+const redisCache = new Redis(); //Create redis client
+
+const io = new Server(httpServer,{
+    cors:{
+        origin:"http://localhost:5500",
+        methods:['GET','POST']
+    }
+});
+
+io.on("connection",(socket)=>{
+    console.log('A user connected ',socket.id);
+    socket.on('setUserId',(userId)=>{
+        redisCache.set(userId, socket.id)
+    })
+    socket.on('getConnectionId',async (userId)=>{
+        console.log(userId);
+        const connId = await redisCache.get(userId);
+        socket.emit('connectionId', connId);
+        const everything = await redisCache.keys('*');
+        console.log("everython",everything);
+        
+    })
+})
+
+app.post('/sendPayLoad',async (req, res)=>{
+    const {userId, payload} = req.body;
+    if(!userId || !payload){
+        res.status(400).send("Invalid Request");
+
+    }
+    const socketId = await redisCache.get(userId);
+    if(socketId){
+        io.to(socketId).emit('submissionPayloadResponse',payload);
+        res.send("payload send successfully");
+    }else{
+        res.status(404).send("User not connected");
+    }
+})
+
+httpServer.listen(8080,()=>{
+    console.log('Server is running on port 8080');
+})
